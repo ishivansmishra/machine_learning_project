b8b6f21d6dd64ed91777f600cb092ca1ff63b4ba
ishivanshmishra <shivanshmishra2701@gmail.com>
2026-04-10 13:52:29 +0530

    final fixing of deployment

--- application.py
+++ application.py
@@ -6,11 +6,11 @@ from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@application.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
